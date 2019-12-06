7d91b0f1099f1cfa2e4b70bde94b1ef961f23a51
Jack Pottage <jack.pottage@gmail.com>
2019-12-06 10:40:04 +0000

    Lists of Sources, Targets, and Transfers per week

--- readHospData.py
+++ readHospData.py
@@ -17,16 +17,32 @@ for i in range(len(data)):
     # currentData.append(data[i][1]) #Site Source
     currentData.append(data[i][2]) #Target
     # currentData.append(data[i][3]) #Site Target
-    currentData.append(data[i][4]) #Transfers
+    currentData.append(int(data[i][4])) #Transfers
     currentYear = int(data[i][5]) #Year
     currentWeek = int(data[i][6]) #Week of Year
     if currentYear == 2014:
         currentWeek = currentWeek-48
     if currentYear == 2015:
-        currentWeek = currentWeek+4
+        currentWeek = currentWeek+3
     if currentYear == 2016:
-        currentWeek = currentWeek + 52 + 4
+        currentWeek = currentWeek + 52 + 3
     currentData.append(currentWeek)
     myData.append(currentData)
 
-print(myData[300])
+#myData form is [Source, Target, Transfers, Week]
+
+Sources = []
+Targets = []
+for i in range(len(myData)):
+    currentSource = myData[i][0]
+    currentTarget = myData[i][1]
+    if currentSource not in Sources:
+        Sources.append(currentSource)
+    if currentTarget not in Targets:
+        Targets.append(currentTarget)
+
+weeklyTransfers = [0]*82
+for i in range(len(myData)):
+    j = myData[i][3]
+    weeklyTransfers[j]+=myData[i][2]
+print((weeklyTransfers))
