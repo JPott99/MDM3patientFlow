aa943282793405c020dfd953f364397bb9688143
Jack Pottage <jack.pottage@gmail.com>
2019-12-13 09:59:32 +0000

    Added zero tracking

--- patientFlow.py
+++ patientFlow.py
@@ -7,11 +7,14 @@ myData,myDataHeaders,sourceTransfers,sourceTransfersHeaders,targetTransfers,targ
 def findMeanProbs(transProb):
     noProb = []
     modelProbLink = []
-
+    modelProbZeros = []
     for i in range(len(transProb)):
         noProb.append(np.array(list(map(float,transProb[i][2:]))))
         modelProbLink.append(transProb[i][:2])
-
+        modelProbZeros.append(0)
+        for j in noProb[i]:
+            if j == 0:
+                modelProbZeros[i]+=1
     avgDiv = len(noProb[0])
     modelProb = []
     for i in range(len(noProb)):
@@ -19,11 +22,12 @@ def findMeanProbs(transProb):
 
     modelProb = np.array(modelProb)
 
-    return(modelProb, modelProbLink)
+    return(modelProb,modelProbZeros,modelProbLink)
 
-modelProb,modelProbLink = findMeanProbs(transProb)
+modelProb,modelProbZeros,modelProbLink = findMeanProbs(transProb)
+
 with open("modelProbabalities.csv",'w') as file:
     writer = csv.writer(file, delimiter=',')
-    writer.writerow(["Source","Target","Probability"])
+    writer.writerow(["Source","Target","Probability","Prob of Zero"])
     for i in range(len(modelProb)):
-        writer.writerow(modelProbLink[i]+[modelProb[i]])
+        writer.writerow(modelProbLink[i]+[modelProb[i],modelProbZeros[i]/82])
