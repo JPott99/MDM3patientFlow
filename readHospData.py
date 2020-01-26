224834453ceaef2bb0c146c3bfa4f18385ade632
Jack Pottage <jack.pottage@gmail.com>
2020-01-26 12:56:40 +0000

    Appended .site to each ward name

--- readHospData.py
+++ readHospData.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     myData = []
     for i in range(len(data)):
         currentData = []
-        currentData.append(data[i][0]) #Source
+        currentData.append(data[i][0]+"."+data[i][1]) #Source
         # currentData.append(data[i][1]) #Site Source
-        currentData.append(data[i][2]) #Target
+        currentData.append(data[i][2]+"."+data[i][3]) #Target
         # currentData.append(data[i][3]) #Site Target
         currentData.append(int(data[i][4])) #Transfers
         currentYear = int(data[i][5]) #Year
