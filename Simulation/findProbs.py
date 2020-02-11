85d3fbace83fe39bd7cafea3ee364ec70f3ed58c
Jack Pottage <jack.pottage@gmail.com>
2020-02-11 13:40:17 +0000

    Added seasonal variation to simulation.

--- Simulation/findProbs.py
+++ Simulation/findProbs.py
@@ -4,7 +4,7 @@ import csv
 
 def findMeanProbs(data):
     # Takes the transferProbability found in readHospData and assigns a single
-    # value weight to each transder.
+    # value weight to each transfer.
     myData,myDataHeaders,sourceTransfers,sourceTransfersHeaders,targetTransfers,targetTransfersHeaders,transfers,tranfersHeadings,transProb,transProbHeaders = data
     noProb = []
     modelProbLink = []
@@ -31,8 +31,62 @@ def findMeanProbs(data):
         output.append(modelProbLink[i]+[modelProb[i]/((1-modelProbZeros[i]/82))])
     return([x for x in output if x != [0.0]])
 
+def findSeasonalProbs(data):
+    myData,myDataHeaders,sourceTransfers,sourceTransfersHeaders,targetTransfers,targetTransfersHeaders,transfers,tranfersHeadings,transProb,transProbHeaders = data
+    noProb = []
+    modelProbLink = []
+    modelProbZeros = []
+    modelProbSeasonSizes = []
+    modelProbSeason =[]
+    for i in range(len(transProb)):
+        noProb.append(np.array(list(map(float,transProb[i][2:]))))
+        modelProbLink.append(transProb[i][:2])
+        modelProbZeros.append([0,0,0,0])
+        modelProbSeasonSizes.append([0,0,0,0])
+        modelProbSeason.append([0,0,0,0])
+        for j in range(len(noProb[i])):
+            if j<2 or (j>=42 and j<55):
+                # Autumn
+                modelProbSeasonSizes[i][0]+=1
+                if noProb[i][j] == 0:
+                    modelProbZeros[i][0]+=1
+                else:
+                    modelProbSeason[i][0]+=noProb[i][j]
+            elif (j>=2 and j<15) or (j>=55 and j < 68):
+                # Winter
+                modelProbSeasonSizes[i][1]+=1
+                if noProb[i][j] == 0:
+                    modelProbZeros[i][1]+=1
+                else:
+                    modelProbSeason[i][1]+=noProb[i][j]
+            elif (j>=15 and j<28) or (j>=68 and j < 81):
+                # Spring
+                modelProbSeasonSizes[i][2]+=1
+                if noProb[i][j] == 0:
+                    modelProbZeros[i][2]+=1
+                else:
+                    modelProbSeason[i][2]+=noProb[i][j]
+            elif (j>=28 and j<41) or (j>=81):
+                # Summer
+                modelProbSeasonSizes[i][3]+=1
+                if noProb[i][j] == 0:
+                    modelProbZeros[i][3]+=1
+                else:
+                    modelProbSeason[i][3]+=noProb[i][j]
+    modelProb =[]
+    for i in range(len(modelProbSeason)):
+        modelProb.append([0,0,0,0])
+        for j in range(len(modelProb[i])):
+            modelProb[i][j] = modelProbSeason[i][j]/modelProbSeasonSizes[i][j]
+            if (modelProbZeros[i][j]!=modelProbSeasonSizes[i][j]):
+                modelProb[i][j] = modelProb[i][j]/(1-modelProbZeros[i][j]/modelProbSeasonSizes[i][j])
+    output = []
+    for i in range(len(modelProb)):
+        output.append(modelProbLink[i]+modelProb[i])
+    return([x for x in output if x != [0.0]])
+
 if __name__=="__main__":
-    output = findMeanProbs(iH.importData())
+    output = findSeasonalProbs(iH.importData())
 
     with open("data/modelProbabalities.csv",'w') as file:
         writer = csv.writer(file, delimiter=',')
--- Simulation/patientFlowSim.py
+++ Simulation/patientFlowSim.py
@@ -6,10 +6,21 @@ import findProbs as fP
 
 # NOTE: readHospData MUST be run once prior to using the simulation.
 
-def targetfromsource(source, data, uniform):
+def targetfromsource(source, data, uniform,currentLoop,startWeek):
     #targetfromsource gives a random target given one person in a given source
     # based on the weightings calculated.
     [sources,targets,probs] = data
+    currentWeek = (startWeek+currentLoop)%52
+    if currentWeek<11:
+        season = 1
+    if currentWeek<24:
+        season = 2
+    if currentWeek<37:
+        season=3
+    if currentWeek<50:
+        season =0
+    else:
+        season=1
     patientSource = source
     patientTarget = source
     # Pick a random number upto 1.
@@ -22,7 +33,7 @@ def targetfromsource(source, data, uniform):
             # probability, which starts at 0, has the weighting of the currently
             # checked source-target pair times a uniformly distributed random
             # number added to it.
-            probability += float(probs[i]) * uniform
+            probability += float(probs[i][season]) * uniform
         if probability>=randomChance:
             # If the weightings of all of the transfers checked upto and
             # including the current one exceed the randomChance, then assign the
@@ -32,17 +43,17 @@ def targetfromsource(source, data, uniform):
             break
     return(patientTarget)
 
-def simulateHospital(wards,wardPatients,loops,data,wardTransfers,uniform,aeinput,currentLoop = 0):
+def simulateHospital(wards,wardPatients,loops,data,wardTransfers,uniform,aeinput,startWeek,currentLoop = 0):
     # Cycles through the simulation for as many loops required. Loops are
     # similar to weeks in the original data.
     while currentLoop < loops:
         wardPatientsCurrent = wardPatients[-1]
-        wardPatients.append(assignment(wards, wardPatientsCurrent,currentLoop, uniform, data,aeinput))
+        wardPatients.append(assignment(wards, wardPatientsCurrent,currentLoop, uniform, data,aeinput,startWeek))
         currentLoop+=1
     return(wardPatients, wardTransfers)
 
 
-def assignment(wards, wardPatientsCurrent,currentLoop, uniform, data, aeinput):
+def assignment(wards, wardPatientsCurrent,currentLoop, uniform, data, aeinput,startWeek):
 
     dh_input = aeinput[0][currentLoop]
     pruh_input = aeinput[1][currentLoop]
@@ -62,7 +73,7 @@ def assignment(wards, wardPatientsCurrent,currentLoop, uniform, data, aeinput):
             # Other patients are assigned individually.
             while counter>0:
                 uniformNo = uniform[currentLoop*len(wards)+i]
-                target = targetfromsource(wards[i],data, uniformNo)
+                target = targetfromsource(wards[i],data, uniformNo,currentLoop,startWeek)
                 wardPatientsFuture[wards.index(target)]+=1
                 counter-=1
                 for j in range(len(sources)):
@@ -76,18 +87,19 @@ def assignment(wards, wardPatientsCurrent,currentLoop, uniform, data, aeinput):
 hospData = iH.importData()
 
 # Get the weightings from findProbs, and put them in a useable format.
-myData = fP.findMeanProbs(hospData)
+myData = fP.findSeasonalProbs(hospData)
 myData = sorted(myData,key=lambda x: -float(x[2]))
 myData = np.array(myData)
 sources = myData[:,0]
 targets = myData[:,1]
-probs = myData[:,2]
+probs = myData[:,2:]
 data = [sources,targets,probs]
 
 # Find the names of each ward.
 wards = sorted(list(set(list(sources) + list(targets))))
 ###############################################################################
 loops = 82 #The number of loops in the main simulation
+startWeek = 48
 ###############################################################################
 # As we don't know the number of patients in each ward, we use the simulation
 # to initialise the hospital. We run it for 5 cycles as that is enough for it
@@ -102,7 +114,7 @@ aeinput = fHi.getInputs(hospData)
 dh_input = np.random.normal(aeinput[0][1],aeinput[0][2],5)
 pruh_input = np.random.normal(aeinput[1][1],aeinput[1][2],5)
 aeinputs = [dh_input,pruh_input]
-wardPatients, wardTransfers = simulateHospital(wards,wardPatientsCurrent,5,data,wardTransfers,uniform,aeinputs)
+wardPatients, wardTransfers = simulateHospital(wards,wardPatientsCurrent,5,data,wardTransfers,uniform,aeinputs,startWeek-6)
 ###############################################################################
 # Having initialised the hospital, we can now run the simulation.
 wardTransfers = np.array([[0]*loops]*len(sources))
@@ -111,7 +123,7 @@ wardPatients = [wardPatients[-1]]
 dh_input = np.random.normal(aeinput[0][1],aeinput[0][2],loops)
 pruh_input = np.random.normal(aeinput[1][1],aeinput[1][2],loops)
 aeinputs = [dh_input,pruh_input]
-wardPatients, wardTransfers = simulateHospital(wards,wardPatients,loops,data,wardTransfers,uniform,aeinputs)
+wardPatients, wardTransfers = simulateHospital(wards,wardPatients,loops,data,wardTransfers,uniform,aeinputs,startWeek)
 transfers = []
 for i in range(len(sources)):
     transfers.append([sources[i]]+[targets[i]]+list(wardTransfers[i]))
