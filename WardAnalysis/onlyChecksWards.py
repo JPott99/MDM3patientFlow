0fe7c78a71c5ba3cd83835b549f5641890ed2fa6
Jack Pottage <jack.pottage@gmail.com>
2020-02-13 17:54:35 +0000

    Added Graphs

--- WardAnalysis/onlyChecksWards.py
+++ WardAnalysis/onlyChecksWards.py
@@ -1,5 +1,15 @@
 import importHospData as iH
 import csv
+from matplotlib import pyplot as plt
+import numpy as np
+from operator import add
+
+def makeListofStringsInt(inputtedMatrix):
+    newMatrix = inputtedMatrix
+    for i in range(len(inputtedMatrix)):
+        newMatrix[i] = list(map(int,inputtedMatrix[i][2:]))
+    return(newMatrix)
+
 myData,myDataHeaders,sourceTransfers,sourceTransfersHeaders,targetTransfers,targetTransfersHeaders,transfers,tranfersHeadings,transProb,transProbHeaders = iH.importData()
 
 wardData = []
@@ -39,6 +49,160 @@ for i in transfers:
 medS = sorted(medS,key=lambda x: x[1])
 surS = sorted(surS,key=lambda x: x[1])
 
+medwardS =np.array([[0]*82]*9)
+for i in medwardS:
+    i = np.array(i)
+for i in range(len(medS)):
+    if "1" in medS[i][0]:
+        medwardS[0]+=np.array(list(map(int,medS[i][2:])))
+    elif "2" in medS[i][0]:
+        medwardS[1]+=np.array(list(map(int,medS[i][2:])))
+    elif "3" in medS[i][0]:
+        medwardS[2]+=np.array(list(map(int,medS[i][2:])))
+    elif "4" in medS[i][0]:
+        medwardS[3]+=np.array(list(map(int,medS[i][2:])))
+    elif "5" in medS[i][0]:
+        medwardS[4]+=np.array(list(map(int,medS[i][2:])))
+    elif "6" in medS[i][0]:
+        medwardS[5]+=np.array(list(map(int,medS[i][2:])))
+    elif "7" in medS[i][0]:
+        medwardS[6]+=np.array(list(map(int,medS[i][2:])))
+    elif "8" in medS[i][0]:
+        medwardS[7]+=np.array(list(map(int,medS[i][2:])))
+    elif "9" in medS[i][0]:
+        medwardS[8]+=np.array(list(map(int,medS[i][2:])))
+medwardT =np.array([[0]*82]*9)
+for i in medwardT:
+    i = np.array(i)
+for i in range(len(medT)):
+    if "1" in medT[i][1]:
+        medwardT[0]+=np.array(list(map(int,medT[i][2:])))
+    elif "2" in medT[i][1]:
+        medwardT[1]+=np.array(list(map(int,medT[i][2:])))
+    elif "3" in medT[i][1]:
+        medwardT[2]+=np.array(list(map(int,medT[i][2:])))
+    elif "4" in medT[i][1]:
+        medwardT[3]+=np.array(list(map(int,medT[i][2:])))
+    elif "5" in medT[i][1]:
+        medwardT[4]+=np.array(list(map(int,medT[i][2:])))
+    elif "6" in medT[i][1]:
+        medwardT[5]+=np.array(list(map(int,medT[i][2:])))
+    elif "7" in medT[i][1]:
+        medwardT[6]+=np.array(list(map(int,medT[i][2:])))
+    elif "8" in medT[i][1]:
+        medwardT[7]+=np.array(list(map(int,medT[i][2:])))
+    elif "9" in medT[i][1]:
+        medwardT[8]+=np.array(list(map(int,medT[i][2:])))
+
+plt.plot(medwardS.transpose())
+plt.grid()
+plt.legend(["1","2","3","4","5","6","7","8","9"],loc="upper left", bbox_to_anchor=(1,1))
+plt.title("Medical Wards as the Transfer Source")
+plt.savefig("data/MedicalS.png")
+plt.clf()
+plt.figure(figsize=(20,10))
+p9 = plt.bar(range(82),medwardS[8],bottom=medwardS[7])
+
+p8 = plt.bar(range(82),medwardS[7],bottom=medwardS[6])
+
+p7 = plt.bar(range(82),medwardS[6],bottom=medwardS[5])
+
+p6 = plt.bar(range(82),medwardS[5],bottom=medwardS[4])
+
+p5 = plt.bar(range(82),medwardS[4],bottom=medwardS[3])
+
+p4 = plt.bar(range(82),medwardS[3],bottom=medwardS[2])
+
+p3 = plt.bar(range(82),medwardS[2],bottom=medwardS[1])
+
+p2 = plt.bar(range(82),medwardS[1],bottom=medwardS[0])
+
+p1 = plt.bar(range(82),medwardS[0])
+
+plt.title("Medical Wards as the Transfer Source")
+
+plt.legend(["9","8","7","6","5","4","3","2","1"],loc="upper left", bbox_to_anchor=(1,1))
+
+plt.savefig("data/MedicalSBar.png")
+plt.clf()
+plt.plot(medwardT.transpose())
+plt.grid()
+plt.legend(["1","2","3","4","5","6","7","8","9"],loc="upper left", bbox_to_anchor=(1,1))
+plt.title("Medical Wards as the Transfer Target")
+plt.savefig("data/MedicalT.png")
+plt.clf()
+plt.figure(figsize=(20,10))
+p9 = plt.bar(range(82),medwardT[8],bottom=medwardT[7])
+
+p8 = plt.bar(range(82),medwardT[7],bottom=medwardT[6])
+
+p7 = plt.bar(range(82),medwardT[6],bottom=medwardT[5])
+
+p6 = plt.bar(range(82),medwardT[5],bottom=medwardT[4])
+
+p5 = plt.bar(range(82),medwardT[4],bottom=medwardT[3])
+
+p4 = plt.bar(range(82),medwardT[3],bottom=medwardT[2])
+
+p3 = plt.bar(range(82),medwardT[2],bottom=medwardT[1])
+
+p2 = plt.bar(range(82),medwardT[1],bottom=medwardT[0])
+
+p1 = plt.bar(range(82),medwardT[0])
+
+
+plt.title("Medical Wards as the Transfer Target")
+plt.legend(["9","8","7","6","5","4","3","2","1"],loc="upper left", bbox_to_anchor=(1,1))
+plt.savefig("data/MedicalTBar.png")
+
+surwardS =np.array([[0]*82]*9)
+for i in surwardS:
+    i = np.array(i)
+for i in range(len(surS)):
+    if "1" in surS[i][0]:
+        surwardS[0]+=np.array(list(map(int,surS[i][2:])))
+    elif "2" in surS[i][0]:
+        surwardS[1]+=np.array(list(map(int,surS[i][2:])))
+    elif "3" in surS[i][0]:
+        surwardS[2]+=np.array(list(map(int,surS[i][2:])))
+    elif "4" in surS[i][0]:
+        surwardS[3]+=np.array(list(map(int,surS[i][2:])))
+    elif "5" in surS[i][0]:
+        surwardS[4]+=np.array(list(map(int,surS[i][2:])))
+    elif "6" in surS[i][0]:
+        surwardS[5]+=np.array(list(map(int,surS[i][2:])))
+    elif "7" in surS[i][0]:
+        surwardS[6]+=np.array(list(map(int,surS[i][2:])))
+surwardT =np.array([[0]*82]*9)
+for i in surwardT:
+    i = np.array(i)
+for i in range(len(surT)):
+    if "1" in surT[i][1]:
+        surwardT[0]+=np.array(list(map(int,surT[i][2:])))
+    elif "2" in surT[i][1]:
+        surwardT[1]+=np.array(list(map(int,surT[i][2:])))
+    elif "3" in surT[i][1]:
+        surwardT[2]+=np.array(list(map(int,surT[i][2:])))
+    elif "4" in surT[i][1]:
+        surwardT[3]+=np.array(list(map(int,surT[i][2:])))
+    elif "5" in surT[i][1]:
+        surwardT[4]+=np.array(list(map(int,surT[i][2:])))
+    elif "6" in surT[i][1]:
+        surwardT[5]+=np.array(list(map(int,surT[i][2:])))
+    elif "7" in surT[i][1]:
+        surwardT[6]+=np.array(list(map(int,surT[i][2:])))
+plt.clf()
+plt.plot(surwardS.transpose())
+plt.grid()
+plt.legend(["1","2","3","4","5","6","7"],loc="upper left", bbox_to_anchor=(1,1))
+plt.title("Surgical Wards as the Transfer Source")
+plt.savefig("data/SurgicalS.png")
+plt.clf()
+plt.plot(surwardT.transpose())
+plt.grid()
+plt.legend(["1","2","3","4","5","6","7"],loc="upper left", bbox_to_anchor=(1,1))
+plt.title("Surgical Wards as the Transfer Target")
+plt.savefig("data/SurgicalT.png")
 with open("data/onlywards.csv",'w') as file:
     writer = csv.writer(file, delimiter=',')
     writer.writerow(["Source", "Target"]+list(range(82)))
